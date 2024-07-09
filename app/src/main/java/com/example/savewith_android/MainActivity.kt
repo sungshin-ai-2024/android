91f06bb2c57b1206e9a15bef0a7b3ccb0f304ccd
user <kjehy2001@daum.net>
2024-07-09 09:22:35 +0900

    MainActivity Commit

--- app/src/main/java/com/example/savewith_android/MainActivity.kt
+++ app/src/main/java/com/example/savewith_android/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.savewith_android__.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
