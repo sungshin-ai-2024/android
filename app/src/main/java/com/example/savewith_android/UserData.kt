a6230eed1fc51024d3ece96b5bc194a83879fd15
user <kjehy2001@daum.net>
2024-08-08 05:41:04 +0900

    Commit Setting(24.08.08)

--- app/src/main/java/com/example/savewith_android/UserData.kt
+++ app/src/main/java/com/example/savewith_android/UserData.kt
@@ -104,23 +104,23 @@ data class ApiResponse(
 //    val profile: Profile
 //)
 
-data class Profile(
-    val signup_name: String,
-    val phone_number: String,
-    val birth_date: String,
-    val sex: String,
-    val zipcode: String,
-    val address: String,
-    val detailed_address: String
-)
+//data class Profile(
+//    val signup_name: String,
+//    val phone_number: String,
+//    val birth_date: String,
+//    val sex: String,
+//    val zipcode: String,
+//    val address: String,
+//    val detailed_address: String
+//)
 
-data class User(
-    val id: Int,
-    val signup_id: String,
-    val signup_name: String,
-    val phone_number: String,
-    val birth_date: String,
-    val sex: String,
-    val address: String,
-    val detailed_address: String
-) // 유저 structure
+//data class User(
+//    val id: Int,
+//    val signup_id: String,
+//    val signup_name: String,
+//    val phone_number: String,
+//    val birth_date: String,
+//    val sex: String,
+//    val address: String,
+//    val detailed_address: String
+//) // 유저 structure
