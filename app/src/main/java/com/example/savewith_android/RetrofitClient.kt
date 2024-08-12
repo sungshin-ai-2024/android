b19c8d84950d953b4a100141de45f2e38db6be29
user <kjehy2001@daum.net>
2024-08-12 22:31:37 +0900

    commit

--- app/src/main/java/com/example/savewith_android/RetrofitClient.kt
+++ app/src/main/java/com/example/savewith_android/RetrofitClient.kt
@@ -8,7 +8,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://your_django_server_url/"
+    private const val BASE_URL = "http://10.0.2.2:8000/"
 
     private val okHttpClient = OkHttpClient.Builder()
         .addInterceptor(AuthInterceptor { getAuthToken() })
